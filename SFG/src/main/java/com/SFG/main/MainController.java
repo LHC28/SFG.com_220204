ff2842cef1f517d830b55fd9cb8a6f0f467f4f2d
이힘찬 <zzangth95@naver.com>
2022-02-07 21:41:33 +0900

    메인페이지 마크업 완료.

--- SFG/src/main/java/com/SFG/main/MainController.java
+++ SFG/src/main/java/com/SFG/main/MainController.java
@@ -1,6 +1,7 @@
 package com.SFG.main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/main")
@@ -10,5 +11,12 @@ public class MainController {
 	@RequestMapping("/entrance")
 	public String entrance() {
 		return "main/entrance";
+	}
+	
+	@RequestMapping("/main_page")
+	public String mainPage(Model model) {
+		
+		model.addAttribute("viewName", "include/mainPage");
+		return "template/layout";
 	}
 }
---
+++ SFG/src/main/webapp/WEB-INF/jsp/include/header.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<header>
+	<div class="d-flex align-items-center justify-content-between">
+		<div class="d-flex align-items-center">
+			<div id="mainLogo" class="bg-danger ml-5"></div>
+			<div class="navBox bg-info"></div>
+		</div>
+		<div class="float-right buttonBox mr-3 bg-success"></div>
+	</div>
+</header>
