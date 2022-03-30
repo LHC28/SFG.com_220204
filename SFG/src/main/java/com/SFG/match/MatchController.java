449e2263b231429cf058e68c1fdca76fb8b91b93
이힘찬 <zzangth95@naver.com>
2022-03-30 21:32:25 +0900

    22.03.30 Date클래스 학습 및 추가 예정

--- SFG/src/main/java/com/SFG/match/MatchController.java
+++ SFG/src/main/java/com/SFG/match/MatchController.java
@@ -10,7 +10,7 @@ public class MatchController {
 
 	@RequestMapping("/match_result_view")
 	public String matchResultView(Model model) {
-		
+//		일자를 맞춰 가져오도록 할 예정(LocalDate 활용)
 		model.addAttribute("viewName", "match/resultView");
 		return "template/layout";
 	}
